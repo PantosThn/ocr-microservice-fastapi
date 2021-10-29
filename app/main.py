e8dfe9a9f44ea384942bc3c35b9413aff5810f7a
PantosThn <pantos.thn@gmail.com>
2021-10-29 20:38:35 +0300

    added authorization tokens for predict view

--- app/main.py
+++ app/main.py
@@ -5,20 +5,28 @@ from functools import lru_cache
 import uuid
 from fastapi import (
     FastAPI,
+    Header,
     HTTPException,
     Depends,
     Request,
     File,
     UploadFile
     )
+import pytesseract
 from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseSettings
+from PIL import Image
 
 #this env file is used for debug purposes
 class Settings(BaseSettings):
+    app_auth_token: str
     debug: bool = False
     echo_active: bool = False
+    app_auth_token_prod: str = None
+    skip_auth: bool = False
+
+
     class Config:
         env_file = ".env"
 
@@ -32,34 +40,54 @@ DEBUG = settings.debug
 
 BASE_DIR = pathlib.Path(__file__).parent
 UPLOAD_DIR = BASE_DIR / "uploads"
-UPLOAD_DIR.mkdir(exist_ok=True)
+
 app = FastAPI()
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
-#Rest API
 @app.get("/", response_class=HTMLResponse) # http GET -> JSON
 def home_view(request: Request, settings:Settings = Depends(get_settings)):
-    #i have to declare the request task above
-    #ussing HTMLResponse to bring back a string(html) and not a json.
-    #FastAPI always returns a json unless changes are made
-    #jinja2 converts html files like python's format.The idea is to take those files and return them as string
-    return templates.TemplateResponse("home.html", {"request": request, "name": "Thanos"})
+    return templates.TemplateResponse("home.html", {"request": request, "abc": 123})
 
-@app.post("/")
-def home_detail_view():
-    return {"hello": "world"}
+def verify_auth(authorization = Header(None), settings:Settings = Depends(get_settings)):
+    """
+    Authorization: Bearer <token>
+    {"authorization": "Bearer <token>"}
+    """
+    if settings.debug and settings.skip_auth:
+        return
+    if authorization is None:
+        raise HTTPException(detail="Invalid endpoint", status_code=401)
+    label, token = authorization.split()
+    if token != settings.app_auth_token:
+        raise HTTPException(detail="Invalid endpoint", status_code=401)
+
+#Rest API
+@app.post("/") #http POST
+async def prediction_view(file:UploadFile = File(...), authorization = Header(None), settings:Settings = Depends(get_settings)):
+    verify_auth(authorization, settings)
+    bytes_str =  io.BytesIO(await file.read()) #read image as a byte stream
+    try:
+        img = Image.open(bytes_str)
+    except:
+        raise HTTPException(detail="Invalid image", status_code=400)
+    preds = pytesseract.image_to_string(img)
+    predictions = [x for x in preds.split("\n")]
+    return {"results": predictions, "original": preds}
 
 
 @app.post("/img-echo/", response_class=FileResponse) #http post
 async def img_echo_view(file:UploadFile = File(...), settings:Settings = Depends(get_settings)):
     if not settings.echo_active:
         raise HTTPException(detail="Invalid endpoint", status_code=400)
+    UPLOAD_DIR.mkdir(exist_ok=True)
     bytes_str =  io.BytesIO(await file.read()) #read image as a byte stream
+    try:
+        img = Image.open(bytes_str)
+    except:
+        raise HTTPException(detail="Invalid image", status_code=400)
     fname = pathlib.Path(file.filename)
     fext = fname.suffix #.jpg
     #use uuid1 which contaings a timestamp for file naming
     dest = UPLOAD_DIR / f"{uuid.uuid1()}{fext}"
-    print(dest)
-    with open(str(dest), 'wb') as out:
-        out.write(bytes_str.read())
+    img.save(dest)
     return dest
